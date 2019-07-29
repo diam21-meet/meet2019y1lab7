7e35f1fdedf3259d2657651d20302eca6afb8465
diam21-meet <diam21@meet.mit.edu>
2019-07-29 05:28:32 -0400

    Stopping here

---
+++ lab6.py
@@ -0,0 +1,7 @@
+def add_number():
+    total=0
+for i in range(1,10):
+    i=i+1
+    
+answer=add_number()
+print(answer)
---
+++ random7.py
@@ -0,0 +1,9 @@
+
+
+counter=0
+for i in range (1,101):
+     counter+=i
+     print (counter)
+print ('the sum is ', counter)
+
+     
